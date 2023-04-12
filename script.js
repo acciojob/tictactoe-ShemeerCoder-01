92c3e34b998043811b810aa05866281661334820
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-12 14:57:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+	function getData(){
+		let user1 = document.getElementById("player-1").value;
+		let user2 = document.getElementById("player-2").value;
+		
+		document.getElementById("gamePage").style.display = "block";
+		document.getElementById("form").style.display = "none";
+	}
